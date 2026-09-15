4557f8880cdd4d32c9a741abd464ad6dc4134a62
Riya <mahrajriya@gmail.com>
2026-09-15 04:20:07 -0700

    dotenv file created

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
 
 const app = express();
 
 
-const port = 8000
+const port = process.env.PORT || 8000;
 
 
 app.get('/', (req, res) => {
